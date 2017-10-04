9a74a7f7bc408c8683fcd40c32129c3f334d4608
user81 <user81@asus>
2017-10-04 13:33:23 +0900

    주간계획에 매출액과달성률 입력 완료.... 클라이언트 테스트 필요

--- src/main/java/com/sfa/controller/PlanWeekController.java
+++ src/main/java/com/sfa/controller/PlanWeekController.java
@@ -48,7 +48,7 @@ public class PlanWeekController {
 			weekVo.setId(authUser.getId());
 			boolean check = weekPlanService.insertWeek(weekVo, dayVo);
 			System.out.println("[controller]"+check);
-			/*if (check) {
+			if (check) {
 				UserVo userVo = userService.getLeader(authUser.getId());
 
 				try {
@@ -72,7 +72,7 @@ public class PlanWeekController {
 				}
 
 				return "redirect:/week/";
-			}*/
+			}
 			return "plan/plan";
 		}
 	}
@@ -139,9 +139,13 @@ public class PlanWeekController {
 		}
 
 		List<DayVo> list = weekPlanService.selectMonth(dayVo);
+		
+		
 		if (list == null) {
 			return JSONResult.fail("제대로 불러오지 못햇습니다.");
-		} else {
+		} 
+		
+		else {
 			return JSONResult.success(list);
 		}
 	}
@@ -166,12 +170,20 @@ public class PlanWeekController {
 			weekVo.setFirst_date(Date);
 		}
 		weekVo = weekPlanService.selectWeek(weekVo);
-
-		if (weekVo == null) {
+        WeekVo temp_weekVo = weekPlanService.selectReport(Date,authUser.getId());
+		System.out.println("[controller]"+temp_weekVo);
+        
+        if (weekVo == null) {
 			JSONResult.error("서버에 error 발생");
-		} else if (weekVo.getMonday() == null) {
-			JSONResult.fail(weekVo);
+		} else if (temp_weekVo==null) {
+			weekVo.setAchive_rank(0.0);
+			weekVo.setWeek_sale((long) 0);	
+		} else
+		{
+			weekVo.setAchive_rank(temp_weekVo.getAchive_rank());
+			weekVo.setWeek_sale(temp_weekVo.getWeek_sale());
 		}
+        System.out.println(weekVo);
 		return JSONResult.success(weekVo);
 	}
 }
